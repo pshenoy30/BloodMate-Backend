3ae2ec7003730ad95446c27267454789f1d8bdbe
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-07-12 01:50:03 +0530

    changed the fetching of users so that a list requestor/donor could be fetched based on the city

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,30 @@
 import User from "../models/userSchema.js"
 
-const getDonorUsers = async (req, res, next) => {
+const getDonorUsers = async (req, res) => {
     try{
-        const users =  await User.find({"userType":"donor"}).sort({"id": 1});
-    
-        return res.status(200).json({
-          data: users
-        });
+        const users =  await User.find({userType:"donor" , city:req.body.city}).sort({"id": 1});
+        if(users.length > 0){
+          return res.status(200).json(users);
+        } else{
+          return res.status(404).send("No donors available")
+        }
       } catch(err) {
         console.log(err);
-        res.status(500).json({ error: 'server error' });
+        res.status(500).send("server error");
       }
 }
 
-const getRequestUsers = async (req, res, next) => {
+const getRequestUsers = async (req, res) => {
     try{
-        const users =  await User.find({"userType":"requestor"}).sort({"id": 1});
-        return res.status(200).json({
-          data: users
-        });
+        const users =  await User.find({userType:"requestor", city:req.body.city}).sort({"id": 1});
+        if(users.length > 0){
+          return res.status(200).json(users);
+        } else{
+          return res.status(404).send("No requestors available")
+        }
       } catch(err) {
         console.log(err);
-        res.status(500).json({ error: 'server error' });
+        res.status(500).send("server error");
       }
 }
 
